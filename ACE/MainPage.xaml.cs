8f760ca49a3f46946041743d019a3bf2d5056d69
Riley <grif0193@algonquinlive.com>
2018-12-04 12:53:44 -0500

    First Full Project Commit

---
+++ ACE/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ACE
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+
+
+
+
+            var onboarded = false;
+
+            if (onboarded == false){
+                onboarded = true;
+
+            }
+        }
+        public void SearchBttn(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new SearchPage());
+        }
+        public void ScanBttn(object sender, EventArgs args)
+        {
+
+        }
+    }
+}
