0b81c14e7dbf389128e838b46fb43bf7f77cac4f
Wael <wael.hussein@cbsa-asfc.gc.ca>
2019-02-03 19:50:10 -0500

    Add POST method to CarImage,HotspotDetail,Manufacturer,Model and ModelYear

--- API/src/main/java/ca/gc/cbsa/mcoe/deltaace/restApi/controller/ModelYearController.java
+++ API/src/main/java/ca/gc/cbsa/mcoe/deltaace/restApi/controller/ModelYearController.java
@@ -4,9 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.gc.cbsa.mcoe.deltaace.restApi.model.ModelYear;
@@ -28,5 +32,10 @@ public class ModelYearController {
     public Optional<ModelYear> id(@PathVariable Long id) {
         return modelYearRepository.findById(id);
     }
+	
+	@PostMapping(value="/add", headers = {"content-type=application/json" }, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody ModelYear addModelYear(@RequestBody ModelYear modelYear) {
+		return modelYearRepository.save(modelYear);
+	}
 
 }
